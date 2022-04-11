60081f55fa6606fe0f857bf33705030fd046406c
casafurix <agnibha10@gmail.com>
2022-04-11 17:01:19 +0530

    commit four (basic app created!)

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
 # backend -> frontend, frontend -> backend (jinja from Flask)
-from crypt import methods
-from flask import Flask, render_template
+# from crypt import methods
+from flask import Flask, render_template, url_for
 import requests
+from flask import request as req
+
 
 app = Flask(__name__)
-@app.route('/', methods=["GET", "POST"])
+
+
+@app.route("/", methods=["GET", "POST"])
 def Index():
     return render_template("index.html")
 
-if __name__ == '__main__':
+
+@app.route("/Summarise", methods=["GET", "POST"])
+def Summarise():
+    if req.method == "POST":
+        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+        headers = {"Authorization": f"Bearer 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"}
+
+        data = req.form["data"]
+
+        minL = 20
+        maxL = 70
+
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+
+        output = query(
+            {
+                "inputs": data,
+                "parameters": {
+                    "min_length": minL,
+                    "max_length": maxL,
+                },
+            }
+        )
+        return render_template("index.html", result=output)
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
     app.run()
